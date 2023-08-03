ca542529a7b567bb9891f871eea748b28e9e0e38
LeoRod17 <matiassotelo171097@gmail.com>
2023-08-03 14:06:31 -0700

    fixing nullable attributes

--- models/place.py
+++ models/place.py
@@ -11,13 +11,13 @@ class Place(BaseModel):
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False,)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False,)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=False)
+    description = Column(String(1024))
     number_rooms = Column(Integer, default=0, nullable=False)
     number_bathrooms = Column(Integer, default=0, nullable=False)
     max_guest = Column(Integer, default=0, nullable=False)
     price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float, nullable=False)
-    longitude = Column(Float, nullable=False)
+    latitude = Column(Float)
+    longitude = Column(Float)
 
 
 class place_amenity():
