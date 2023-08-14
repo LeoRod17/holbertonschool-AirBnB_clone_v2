f68833159f934fb3c6de5697cd185da1037fd97e
root <matiassotelo171097@gmail.com>
2023-08-14 10:56:57 -0700

    going home

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" A basic flask app it should display texts"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def Hello():
+    """a function that returns the hello in the route"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def HBNB():
+    "a funtion that returns HBNB"
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def CisF(text):
+    "a funtion that returns HBNB"
+    newTxt = text.replace("_", " ")
+    return f'C {newTxt}'
+
+
+@app.route("/python/<text>")
+def pythonisCool(text= "is cool"):
+    "a funtion that returns HBNB"
+    newTxt = text.replace("_", " ")
+    return f'Python {newTxt}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
