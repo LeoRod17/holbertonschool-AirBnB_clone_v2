b950578bd1ecdec30eb9a4b0bb7d6a4f9902b81b
root <matiassotelo171097@gmail.com>
2023-08-14 09:11:23 -0700

    task 0 of Flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" A basic flask app it should display Hello HBNB!"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def Hello():
+    """a function that returns the hello in the route"""
+    return "Hello HBNB!"
+
+if __name__ == '__main__': 
+   app.run(host='0.0.0.0', port=5000)
