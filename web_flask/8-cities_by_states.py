b892039dda640e69ba08535f5009f0c329564ccb
root <matiassotelo171097@gmail.com>
2023-08-16 10:44:19 -0700

    task 9 going home

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" A basic flask app it should display the list of Cities in States"""
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.city import City
+from models.state import State
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def tear(r):
+    storage.close()
+
+
+@app.route("/", strict_slashes=False)
+def Welcome():
+    """a function that shows a welcoming message"""
+    return "Welcome to AirBnB"
+
+
+@app.route("/states_list", strict_slashes=False)
+def LC():
+    """a function that returns a number"""
+    dic = storage.all(State)
+    return render_template("8-cities_by_states.html", state=dic)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
